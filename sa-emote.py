a1311d167ec55772c4a6594f691dc5c21dc2ebd7
Zeebo <leterip@gmail.com>
2009-10-10 02:02:24 -0400

    modified to fix bug where smilie was inserted after cursor

--- sa-emote.py
+++ sa-emote.py
@@ -4,13 +4,19 @@ from waveapi import model
 from waveapi import robot
 from waveapi import document
 
+def OnDocumentChanged(properties, context):
+  for blip in context.GetBlips():
+    UpdateBlip(blip, chop=-1)
+
 def OnBlipSubmitted(properties, context):
   for blip in context.GetBlips():
     UpdateBlip(blip)
 
-def UpdateBlip(blip):
+def UpdateBlip(blip, chop=0):
   doc = blip.GetDocument()
   doc_text = doc.GetText()
+  if chop < 0:
+    doc_text = doc_text[:chop]
   
   updates = []
   for text, src in smilies.smilies:
@@ -18,7 +24,7 @@ def UpdateBlip(blip):
     while doc_text.find(text, i) != -1:
       i = doc_text.find(text, i)
       updates.append((i, text,
-                      "http://sa-emote.appspot.com/assets/emotes/%s" % src))
+                      "http://zeebo-devel.appspot.com/assets/emotes/%s" % src))
       i += len(text)
   
   updates.sort()
@@ -30,7 +36,8 @@ def UpdateBlip(blip):
 if __name__ == '__main__':
   myRobot = robot.Robot('sa-emote', 
       image_url='http://sa-emote.appspot.com/assets/awesome.png',
-      version='2',
+      version='3',
       profile_url='http://sa-emote.appspot.com/')
-  myRobot.RegisterHandler(events.DOCUMENT_CHANGED, OnBlipSubmitted)
+  myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
+  myRobot.RegisterHandler(events.DOCUMENT_CHANGED, OnDocumentChanged)
   myRobot.Run()
